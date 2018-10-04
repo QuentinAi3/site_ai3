124870da3791d6cd80ee1e3aba1f44c421acc48a
Quentin Lemardelle <quentin.lemardelle@ai3.fr>
2018-10-04 15:48:56 +0200

    desk

---
+++ inc/nav.inc.php
@@ -0,0 +1,61 @@
+	
+<nav class="container-fluid">
+	<div class="row dropdown">
+		<div class="col-4 offset-md-1 col-md-1 align-self-center">
+			<img class="logorouge" src="images/logo_rouge.svg" alt="">
+		</div>
+			<ul class="d-none col-8 menu menu-desktop align-self-center d-md-flex">
+				<li class="col">
+				<a href="#">Accueil</a>
+				</li>
+				<li id="dropdownMenuButton" class="col dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><a href="#">Nos expertises</a></li>
+				<ul class="navdown dropdown-menu" aria-labelledby="dropdownMenuButton">
+					<li class="offset-1 col align-middle"><a class="dropdown-item" href="#">transformation &amp;<br> accompagnement du changement</a></li>
+					<li class="col align-middle"><a class="dropdown-item" href="#">experiences utilisateur</a></li>
+					<li class="col align-middle"><a class="dropdown-item" href="#">environnement de<br>travail digital</a></li>
+					<li class="col align-middle"><a class="dropdown-item" href="#">applications métiers</a></li>
+					<li class="col align-middle"><a class="dropdown-item" href="#">data &amp; ia</a></li>
+					<li class="col align-middle"><a class="dropdown-item" href="#">infrastructure</a></li>
+					<li class="col align-middle"><a class="dropdown-item" href="#">services managés &amp;<br>externalisation</a></li>
+					
+				  </ul>
+				<li class="col"><a href="#">Nos réalisations</a></li>
+				<li class="col"><a href="#">Nos publications</a></li>
+				<li class="col"><a href="#">Nous connaître</a></li>
+				<li class="col"><a href="#">Nos évènements</a></li>
+				<li class="col"><a href="#">Contact</a></li>
+			</ul>
+		<div class="back-cover">
+			<div class="row">
+				<div class="offset-4 col-4 menuToggle align-self-center">
+					<div id="" class="rond rond-open">
+						<div>
+							<span></span>
+							<span></span>
+						</div>
+					</div>
+				</div>
+			</div>	
+			<ul class="offset-1 col-10 menu">
+				<li><a href="#">Accueil</a></li>
+				<li><a href="#">Nos expertises</a></li>
+				<li><a href="#">Nos réalisations</a></li>
+				<li><a href="#">Nos publications</a></li>
+				<li><a href="#">Nous connaître</a></li>
+				<li><a href="#">Nos évènements</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</div>
+		<div class="d-md-none col-4 menuToggle align-self-center">
+			<div id="" class="rond">
+				<div>
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+			</div>
+		</div>
+		<div class="col-4 col-md-1 team align-self-center">rejoignez <br/>nos équipes
+		</div>
+	</div>
+</nav>
