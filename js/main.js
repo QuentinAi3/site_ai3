bdf160d98f081f0c0aacc8900b0a4e6dd0027559
Quentin Lemardelle <quentin.lemardelle@ai3.fr>
2018-10-15 14:31:56 +0200

    Page rejoindre à terminer

--- js/main.js
+++ js/main.js
@@ -1,17 +1,54 @@
 $(document).ready(function(){
 
-	$(".cache").hide();
+
 	$(".rond").click( function() {
-		$(".cache").hide();
-		var c = $(this).attr('class').split(' ')[0]; //Get First Class
-	$('.back-cover').css('display',"block").fadeIn('slow').show();
+
+	$('.back-cover').show("slow");
 	});
 
-	$('.rond-open').on('click',function(){
-		$('.back-cover').css('display','none');
+	$('.rond-open').on('click touch',function(){
+		$('.back-cover').hide("slow");
 	});
-	$('.rond-open').on('click',function(){
-		$('.cache').hide();
+	$('.rond-open').on('click touch',function(){
+		$('.cache').hide("slow");
 	});
+
+      
+    $('.btnpolitique').on('click touch',function(){
+      $('.job').hide("slow");
+      $('.politique-rh').show("slow");
+    });
+	$('.btnoffre').on('click touch',function(){
+      $('.politique-rh').hide("slow");
+      $('.job').show("slow");
+    });
+
+	
+	
+/** ANIME POSTES **/
+
+	
+	$(".postes").on('click touch',function(){
+  var columns_container = $(".dynamic-columns");
+  if (!columns_container.hasClass("expanded")) {
+    $(".dynamic-columns .liste-postes").removeClass("col-md-9");
+    $(".dynamic-columns .liste-postes").addClass("col-md-3");
+	  
+	 $(".desc-postes").removeClass("d-none");
+    $(".desc-postes").addClass("d-inline-block");
+
+    columns_container.toggleClass("expanded");
+  }
+  else {
+    $(".dynamic-columns .liste-postes").removeClass("col-md-3");
+    $(".dynamic-columns .liste-postes").addClass("col-md-9");
+	
+	  $(".desc-postes").removeClass("d-inline-block");
+    $(".desc-postes").addClass("d-none");
+
+    columns_container.toggleClass("expanded");
+  }
+});
+	
 });
 
